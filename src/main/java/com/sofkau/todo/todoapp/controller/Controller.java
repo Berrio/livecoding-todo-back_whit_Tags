7a3c115f832ea3397c2c83661cddf8e781ff9e97
JonatanBerrioRoman <berrioromanjonatan@gmail.com>
2022-05-13 20:52:21 -0500

    tag able to post

--- src/main/java/com/sofkau/todo/todoapp/controller/Controller.java
+++ src/main/java/com/sofkau/todo/todoapp/controller/Controller.java
@@ -2,9 +2,11 @@ package com.sofkau.todo.todoapp.controller;
 
 import com.sofkau.todo.todoapp.dto.CategoryDto;
 import com.sofkau.todo.todoapp.dto.NoteDto;
+import com.sofkau.todo.todoapp.dto.TagDto;
 import com.sofkau.todo.todoapp.entity.Category;
 import com.sofkau.todo.todoapp.service.CategoryService;
 import com.sofkau.todo.todoapp.service.NoteService;
+import com.sofkau.todo.todoapp.service.TagService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -21,6 +23,9 @@ public class Controller {
     @Autowired
     private NoteService noteService;
 
+    @Autowired
+    private TagService tagService;
+
     @GetMapping("get/all/categories")
     public List<CategoryDto> getAllCategories(){
         return categoryService.findAllCategories();
@@ -34,6 +39,11 @@ public class Controller {
     @PostMapping("save/note")
     public NoteDto saveNote(@RequestBody NoteDto note){
         return noteService.createNote(note);
+    }
+
+    @PostMapping("save/tag")
+    public TagDto saveCategory(@RequestBody TagDto tagDto){
+        return tagService.createTag(tagDto);
     }
 
     @PutMapping("update/note")
---
+++ src/main/java/com/sofkau/todo/todoapp/dto/TagDto.java
@@ -0,0 +1,11 @@
+package com.sofkau.todo.todoapp.dto;
+
+import lombok.Data;
+
+@Data
+public class TagDto {
+
+    private Long id;
+    private String tag;
+    private String noteId;
+}
